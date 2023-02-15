f60b9e33d77dda9f3c875f57c63a5b1a72f27573
Philip Sole <philip.sole@gmail.com>
2023-02-16 00:32:02 +1300

    env stuff working and with nodemon

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { clusterApiUrl, Connection, PublicKey } = require("@solana/web3.js");
+require('dotenv').config();
 
 const express = require('express')
 const app = express()
+
+const { clusterApiUrl, Connection, PublicKey, Keypair } = require("@solana/web3.js");
+const bs58 = require('bs58');
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
@@ -9,8 +12,7 @@ app.all('/', (req, res) => {
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
     const wallet = new PublicKey(process.env.BCC_DEV_WALLET_ADDRESS);
-
-    console.log(wallet);
+    const feePayer = Keypair.fromSecretKey(bs58.decode(process.env.BCC_DEV_WALLET_PRIVATE));
 
     res.send('Yoza!')
 })
