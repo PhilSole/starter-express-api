c6a64ec25a7526d06bb2136693ed00f4d3d17b15
Philip Sole <philip.sole@gmail.com>
2023-02-15 22:53:44 +1300

    import solana

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
+
+    const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+
+    console.log(connection);
+
     res.send('Yoza!')
 })
+
 app.listen(process.env.PORT || 3000)
